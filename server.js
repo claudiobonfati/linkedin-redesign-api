7b85e11d6908ede0a659af069f747c5a93ab21dd
Claudio Bonfati <claudio_bonfati@outlook.com>
2020-12-21 13:11:11 -0300

    Enabled CORS from localhost

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const jsonGraphqlExpress = require('json-graphql-server').default;
 const data = require('./db.js');
+const cors = require('cors');
 
 const port = process.env.PORT;
 const app = express();
 
+// Enable cors
+var corsOptions = {
+  origin: 'http://localhost:3001',
+  credentials: false
+};
+
+app.use(cors(corsOptions));
 app.use('/graphql', jsonGraphqlExpress(data));
+
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`)
 });
